e63c2d0102521a52c1d4eb156eb595736f140349
Robin Eriksson <ro1067er-s@student.lu.se>
2023-10-31 14:56:43 +0100

    Rebased, pull, pushed, fixedCSV parser library

--- main.cxx
+++ main.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "argh.h"
+#include "csv.h"
 
 int main(int, char *argv[]) {
   argh::parser cmdl(argv);
@@ -13,7 +14,20 @@ int main(int, char *argv[]) {
   if (cmdl[{"-i", "--input-file"}]) {
     std::string FileName{argv[2]};
     // std::cout << FileName << std::endl;
+    std::cout << FileName << std::endl;
+    io::CSVReader<4> in(FileName);
+    in.read_header(io::ignore_extra_column, "day", "year", "month", "measurement");
+    int day; int year; int month; double measurement;
+    while(in.read_row(day, year, month, measurement)){
+      std::cout << day << " " << year<< " " << month<< " " << measurement << std::endl;
+    }
   }
+
+  
+
+
+
+
 
   // std::cout << "I am just a code template, you need to implement the "
   //              "functionality you want to use yourself!"
